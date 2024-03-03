8b787d3ce6444dfb871948b449c3b22e2232b611
Bee <huuphuoc252513@gmail.com>
2024-03-04 09:34:20 +1100

    use another news api

--- hooks/news/useNews.ts
+++ hooks/news/useNews.ts
@@ -1,5 +1,12 @@
 import { useEffect, useState } from "react";
 
+// export type News = {
+//   title: string
+//   description?: string
+//   url: string
+//   author?: string
+//   publishedAt: string
+// }
 export type News = {
   title: string
   description?: string
@@ -13,23 +20,27 @@ const useNews = (category: string) => {
   const [loading, setLoading] = useState<boolean>(false);
   const params = new URLSearchParams({
     category,
-    pageSize: "10",
     language: "en",
   })
 
   useEffect(() => {
     const fetchNews = async () => {
       setLoading(true);
-      const response = await fetch(`https://newsapi.org/v2/top-headlines?${params.toString()}&apiKey=${process.env.NEXT_PUBLIC_NEWS_API_KEY}`);
+      const response = await fetch(`https://api.thenewsapi.com/v1/news/all?${params.toString()}&api_token=${process.env.NEXT_PUBLIC_THE_NEWS_API_KEY}`);
       const data = await response.json();
-      if (data.status !== "ok") throw new Error(data.message);
-      setNews(data.articles.map((article: any) => ({
-        title: article.title,
-        description: article.description,
-        url: article.url,
-        author: article.author,
-        publishedAt: new Date(article.publishedAt).toLocaleDateString(),
-      })))
+      // if (data.status !== "ok") throw new Error(data.message);
+      console.log({data})
+      setNews([
+        ...news,
+        ...data.data.map((article: any) => ({
+          title: article.title,
+          description: article.description,
+          url: article.url,
+          author: article.source,
+          publishedAt: new Date(article.published_at).toLocaleDateString(),
+        }))
+      ])
+      
       setLoading(false);
     };
 
